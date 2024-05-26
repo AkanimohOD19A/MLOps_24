31b91bfdbf071c907f4535283e3782bddf673cb8
Akan Daniel <danielamahtoday@gmail.com>
2024-05-26 21:25:58 +0100

    completed_wk2

---
+++ wk2/train.py
@@ -0,0 +1,44 @@
+import os
+import pickle
+from math import sqrt
+import click
+import mlflow
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+# Set the experiment name
+mlflow.set_experiment("sklearn-init")
+
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+@click.command()
+@click.option(
+    "--data_path",
+    default="./output",
+    help="Location where the processed NYC taxi trip data was saved"
+)
+def run_train(data_path: str):
+    # Enable autolog
+    mlflow.sklearn.autolog()
+    with mlflow.start_run():
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        mse = mean_squared_error(y_val, y_pred, squared=False)
+        rmse = sqrt(mse)
+
+
+if __name__ == '__main__':
+    run_train()
+
+# mlflow server --backend-store-uri sqlite:///mlflow.db  --default-artifact-root ./artifacts  --host 0.0.0.0
