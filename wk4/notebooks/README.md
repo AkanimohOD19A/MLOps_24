65f30533542ddb42e031f2eebc412c536b6462c4
Akan Daniel <danielamahtoday@gmail.com>
2024-06-10 07:28:31 +0100

    wk 4 homework

---
+++ wk4/notebooks/README.md
@@ -0,0 +1,58 @@
+### Week 4 Homework
+
+This week we learn about creating a prediction service, which can take many dimensions, __Batch__, __Stream__, ..
+
+For the homework we deal with creating a local solution.
+
+#### Q1
+We simply modify the code in a previous notebook to fetch the standard deviation of the predicted duration of March  2023 data
+
+#### Q2 - Q3
+Covered in the starter.ipynb notebook
+
+#### Q4 Pipenv 
+Here, you have to 
+`pip install pipenv`
+`pipenv --python 3.x` (put the appropriate python version in)
+`pipenv install <package>==<package.version`
+to then install the neccessary packages like os for sklearn
+
+#### Q5: Parametrize the script
+Parametrize here means that we use/set system variable that can accept values that it can now run from the terminal.
+
+See how this is done in the `script.py` script, particularly this is set for __taxi-type__, __year__ and __month__ like so:
+```
+taxi_type = sys.argv[1] #yellow
+year = int(sys.argv[2]) #2024
+month = int(sys.argv[3]) #3
+```
+after you have the script, run the following in the terminal:
+`python script.py yellow 2023 4` - a print statement would handle the mean predicted duration for April 2023
+
+#### Q6: Docker container
+Here, we breathe a sign of relief, last question.
+[ChatGPT]
+A Docker container is a lightweight, standalone, and executable package of software that includes everything needed to run an application. It packages up code and all its dependencies so the application runs quickly and reliably from one computing environment to another.
+
+In the same directory as you have been working all along;
+We simply create a `Dockerfile`, launch our Docker Engine - locally and create a `requirements.txt` that has a list and versions of your packages. 
+
+Update the Dockerfile like you would see in this repo.
+
+Run:
+To build your docker image, replace "<name>" to any name of your choice, here "my-model" is used.
+` docker build -t <name> .`
+`docker run my-model`
+```
+$ docker run my-model
+reading the data from https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-05.parquet...
+loading the model with RUN_ID=model.bin...
+applying the model...
+Q6. Mean of predictions: 0.192
+saving the result to ../output_files/yellow/2023-05.parquet...
+```
+That's it!
+
+Let me know if you have any challenges.
+
+**NB** Difference between __starter.py__ and __script.py__ is the latter is built to have the pre-built model from the **FROM agrigorev/zoomcamp-model:mlops-2024-3.10.13-slim** docker image.
---
+++ wk4/notebooks/starter.py
@@ -0,0 +1,105 @@
+## Load Libraries
+import os
+import sys
+
+import pickle
+import pandas as pd
+from datetime import date
+
+## Load Model function
+def load_model(model_pth):
+    with open(model_pth, 'rb') as f:
+        dv, model = pickle.load(f)
+
+    return dv, model
+
+
+categorical = ["PULocationID", "DOLocationID"]
+
+## Load Data function
+def read_data(filename):
+    df = pd.read_parquet(filename)
+
+    df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
+    df['duration'] = df.duration.dt.total_seconds() / 60
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
+
+    df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
+    
+    return df
+
+def save_result(df, y_pred, output_file, year, month):
+    df["ride_id"] = f"{year:04d}/{month:02d}_" + df.index.astype("str")
+
+    ## Save predictions to dataframe
+    df_results = pd.DataFrame(columns = ["predictions", "ride_id"])
+    df_results["predictions"] = y_pred
+    df_results["ride_id"] = df["ride_id"]
+    
+    df_results.to_parquet(
+        output_file,
+        engine = 'pyarrow',
+        compression = None,
+        index = False
+    )
+
+def apply_model(input_file, model_file, output_file, year, month):
+
+    print(f"reading the data from {input_file}...")
+    df = read_data(input_file)
+
+    ## Isolating stated catgeories, call dict vectorizer and predict
+    dicts = df[categorical].to_dict(orient='records')
+
+    print(f'loading the model with RUN_ID={model_file}...')
+    retrv_model = load_model(model_file)
+    dv = retrv_model[0]
+    model = retrv_model[1]
+
+    X_val = dv.transform(dicts)
+
+    print(f'applying the model...')
+    y_pred = model.predict(X_val)
+    
+    ## Output 1
+    print(f"Q5. Mean of predictions: {round(y_pred.mean(), 3)}")
+
+    print(f'saving the result to {output_file}...')
+    save_result(df, y_pred, output_file, year, month)
+
+    return output_file
+
+
+def run():
+    taxi_type = sys.argv[1] #yellow
+    year = int(sys.argv[2]) #2024
+    month = int(sys.argv[3]) #3
+
+    ## Fetch Data
+    input_file = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{taxi_type}_tripdata_{year:04d}-{month:02d}.parquet" 
+
+    model_pth = "../models/model.bin"
+
+    output_pth = "../output_files"
+    if not os.path.exists(f"{output_pth}/{taxi_type}"):
+        os.makedirs(f"{output_pth}/{taxi_type}")
+
+    output_file = f"{output_pth}/{taxi_type}/{year:04d}-{month:02d}.parquet"
+
+    apply_model(
+        input_file = input_file,
+        model_file = model_pth,
+        output_file = output_file,
+        year = year,
+        month = month
+    )
+
+    # print(get_ipython().system(" ls -lh ../output_files/* | awk '{print $5, $9}'"))
+    # $5, $9: These represent the 5th and 9th fields of each line.
+    # (i.e., in kilobytes (K), megabytes (M), gigabytes (G), etc.)
+
+
+
+if __name__ == '__main__':
+    run()
